cc7da9aadf262cb602306df10682289d6d7140ac
Trebolll <M-or-ev@yandex.ru>
2023-02-27 21:28:24 +0700

    init commit

---
+++ dispather/src/main/java/service/UpdateProducer.java
@@ -0,0 +1,12 @@
+package service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import javax.annotation.Resource;
+
+
+public interface UpdateProducer {
+    void produce(String rabbitQueue, Update update);
+}
